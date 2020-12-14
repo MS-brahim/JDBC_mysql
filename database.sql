4606c734dae02d3a532f82dbe25c363e718f850d
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2020-12-14 10:00:02 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE [IF NOT EXISTS] test;
-CREATE TABLE youcode_test (
+CREATE DATABASE IF NOT EXISTS test;
+CREATE TABLE IF NOT EXISTS youcode_test (
     id              INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name            VARCHAR(10) NOT NULL,
     description     VARCHAR(30) NOT NULL,
-    CIN             VARCHAR(8) NOT NULL,
-);
+    CIN             VARCHAR(8) NOT NULL
+)
